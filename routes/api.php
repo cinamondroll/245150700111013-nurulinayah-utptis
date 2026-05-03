2db7708ced26b945baa41a53890fb8f35aedee17
misuaa <nurulinayahh19@student.ub.ac.id>
2026-05-03 19:49:37 +0700

    Fix: library api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
@@ -9,3 +10,7 @@ Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::patch('/products/{id}', [ProductController::class, 'partialUpdate']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
